e3ebc1b5194501261948b77a7ef0284da5eebd57
Daniel Klabbers <daniel+github@klabbers.email>
2015-09-04 16:02:08 +0200

    attempt to create dropdown

--- js/admin/src/components/DefaultGroupSettingsModal.js
+++ js/admin/src/components/DefaultGroupSettingsModal.js
@@ -1,12 +1,29 @@
 import Modal from 'flarum/components/Modal';
+import Group from 'flarum/models/Group';
 import Button from 'flarum/components/Button';
 import saveConfig from 'flarum/utils/saveConfig';
+import Dropdown from 'flarum/components/Dropdown';
+import GroupBadge from 'flarum/components/GroupBadge';
 
 export default class DefaultGroupSettingsModal extends Modal {
   constructor(...args) {
     super(...args);
 
+    const user = app.session.user;
+
     this.defaultGroup = m.prop(app.config['hyn.default_group.group'] || '');
+    //this.group = app.store.all('groups').findById(this.defaultGroup);
+    this.groups = {};
+    app.store.all('groups')
+        .filter(group => [Group.GUEST_ID].indexOf(group.id()) === -1)
+        .map(group => Button.component({
+              children: [GroupBadge.component({group, label: null}), ' ', group.namePlural()],
+              icon: group.id() == this.defaultGroup ? 'check' : true,
+              onclick: (e) => {
+                e.stopPropagation();
+                this.toggle(group.id());
+              }
+        }));
   }
 
   className() {
@@ -21,10 +38,19 @@ export default class DefaultGroupSettingsModal extends Modal {
     return (
       <div className="Modal-body">
         <div className="Form">
+
+
           <div className="Form-group">
             <label>Default group</label>
-            <input className="FormControl" value={this.defaultGroup()} oninput={m.withAttr('value', this.defaultGroup)}/>
+            <div className="Form-group">
+              {Dropdown.component({
+                  key: 'default_group',
+                  children: [this.groups]
+              })}
+
+            </div>
           </div>
+
           <div className="Form-group">
             {Button.component({
               type: 'submit',
