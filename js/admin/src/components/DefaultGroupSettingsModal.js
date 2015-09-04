50b0ed96ad8d324eeb24afcb85cb8a787f3ad971
Daniel Klabbers <daniel+github@klabbers.email>
2015-09-04 16:42:43 +0200

    todo: fix label and post action has to save

--- js/admin/src/components/DefaultGroupSettingsModal.js
+++ js/admin/src/components/DefaultGroupSettingsModal.js
@@ -2,7 +2,7 @@ import Modal from 'flarum/components/Modal';
 import Group from 'flarum/models/Group';
 import Button from 'flarum/components/Button';
 import saveConfig from 'flarum/utils/saveConfig';
-import Dropdown from 'flarum/components/Dropdown';
+import SelectDropdown from 'flarum/components/SelectDropdown';
 import GroupBadge from 'flarum/components/GroupBadge';
 
 export default class DefaultGroupSettingsModal extends Modal {
@@ -11,18 +11,14 @@ export default class DefaultGroupSettingsModal extends Modal {
 
     const user = app.session.user;
 
-    this.defaultGroup = m.prop(app.config['hyn.default_group.group'] || '');
-    //this.group = app.store.all('groups').findById(this.defaultGroup);
-    this.groups = {};
-    app.store.all('groups')
+    this.defaultGroup = app.config['hyn.default_group.group'] || Group.MEMBER_ID;
+    //this.group = app.store.all('groups').findById(this.defaultGroup
+    this.groups = app.store.all('groups')
         .filter(group => [Group.GUEST_ID].indexOf(group.id()) === -1)
         .map(group => Button.component({
-              children: [GroupBadge.component({group, label: null}), ' ', group.namePlural()],
-              icon: group.id() == this.defaultGroup ? 'check' : true,
-              onclick: (e) => {
-                e.stopPropagation();
-                this.toggle(group.id());
-              }
+            children: [GroupBadge.component({group, label: null}), ' ', group.namePlural()],
+            icon: group.id() == this.defaultGroup ? 'check' : true,
+            active: group.id() == this.defaultGroup
         }));
   }
 
@@ -43,9 +39,9 @@ export default class DefaultGroupSettingsModal extends Modal {
           <div className="Form-group">
             <label>Default group</label>
             <div className="Form-group">
-              {Dropdown.component({
-                  key: 'default_group',
-                  children: [this.groups]
+              {SelectDropdown.component({
+                  key: 'hyn.default_group.group',
+                  children: this.groups
               })}
 
             </div>
@@ -63,20 +59,20 @@ export default class DefaultGroupSettingsModal extends Modal {
       </div>
     );
   }
-
-  onsubmit(e) {
-    e.preventDefault();
-
-    this.loading = true;
-
-    saveConfig({
-      'hyn.default_group.group': this.defaultGroup()
-    }).then(
-      () => this.hide(),
-      () => {
-        this.loading = false;
-        m.redraw();
-      }
-    );
-  }
+  //
+  //onsubmit(e) {
+  //  e.preventDefault();
+  //
+  //  this.loading = true;
+  //
+  //  saveConfig({
+  //    'hyn.default_group.group': this.defaultGroup()
+  //  }).then(
+  //    () => this.hide(),
+  //    () => {
+  //      this.loading = false;
+  //      m.redraw();
+  //    }
+  //  );
+  //}
 }
