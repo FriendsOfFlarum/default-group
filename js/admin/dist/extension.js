7dc4d8c8281c0b0e0f693f27fc18df8bf8ea1edd
Daniel Klabbers <daniel+hyn@klabbers.email>
2015-12-01 22:20:22 +0100

    fixed default group

--- js/admin/dist/extension.js
+++ js/admin/dist/extension.js
@@ -1,124 +1,69 @@
-System.register('hyn/default-group/components/DefaultGroupSettingsModal', ['flarum/components/Modal', 'flarum/models/Group', 'flarum/components/Button', 'flarum/utils/saveConfig', 'flarum/components/SelectDropdown', 'flarum/components/GroupBadge'], function (_export) {
-  'use strict';
+System.register('hyn/default-group/components/DefaultGroupSettingsModal', ['flarum/components/SettingsModal', 'flarum/models/Group', 'flarum/components/SelectDropdown'], function (_export) {
+    'use strict';
 
-  var Modal, Group, Button, saveConfig, SelectDropdown, GroupBadge, DefaultGroupSettingsModal;
-  return {
-    setters: [function (_flarumComponentsModal) {
-      Modal = _flarumComponentsModal['default'];
-    }, function (_flarumModelsGroup) {
-      Group = _flarumModelsGroup['default'];
-    }, function (_flarumComponentsButton) {
-      Button = _flarumComponentsButton['default'];
-    }, function (_flarumUtilsSaveConfig) {
-      saveConfig = _flarumUtilsSaveConfig['default'];
-    }, function (_flarumComponentsSelectDropdown) {
-      SelectDropdown = _flarumComponentsSelectDropdown['default'];
-    }, function (_flarumComponentsGroupBadge) {
-      GroupBadge = _flarumComponentsGroupBadge['default'];
-    }],
-    execute: function () {
-      DefaultGroupSettingsModal = (function (_Modal) {
-        babelHelpers.inherits(DefaultGroupSettingsModal, _Modal);
+    var SettingsModal, Group, SelectDropdown, DefaultGroupSettingsModal;
+    return {
+        setters: [function (_flarumComponentsSettingsModal) {
+            SettingsModal = _flarumComponentsSettingsModal['default'];
+        }, function (_flarumModelsGroup) {
+            Group = _flarumModelsGroup['default'];
+        }, function (_flarumComponentsSelectDropdown) {
+            SelectDropdown = _flarumComponentsSelectDropdown['default'];
+        }],
+        execute: function () {
+            DefaultGroupSettingsModal = (function (_SettingsModal) {
+                babelHelpers.inherits(DefaultGroupSettingsModal, _SettingsModal);
 
-        function DefaultGroupSettingsModal() {
-          babelHelpers.classCallCheck(this, DefaultGroupSettingsModal);
+                function DefaultGroupSettingsModal() {
+                    babelHelpers.classCallCheck(this, DefaultGroupSettingsModal);
+                    babelHelpers.get(Object.getPrototypeOf(DefaultGroupSettingsModal.prototype), 'constructor', this).apply(this, arguments);
+                }
 
-          for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
-            args[_key] = arguments[_key];
-          }
+                babelHelpers.createClass(DefaultGroupSettingsModal, [{
+                    key: 'className',
+                    value: function className() {
+                        return 'DefaultGroupSettingsModal Modal--small';
+                    }
+                }, {
+                    key: 'title',
+                    value: function title() {
+                        return 'Default group Settings';
+                    }
+                }, {
+                    key: 'form',
+                    value: function form() {
+                        this.defaultGroup = this.setting('hyn.default_group.group') || Group.MEMBER_ID;
+                        this.groups = app.store.all('groups').filter(function (group) {
+                            return [Group.GUEST_ID].indexOf(group.id()) === -1;
+                        });
+                        return [m(
+                            'div',
+                            { className: 'Form-group' },
+                            m(
+                                'label',
+                                null,
+                                'Default group'
+                            ),
+                            m(
+                                'select',
+                                { name: 'defaultGroup', bidi: this.defaultGroup },
+                                this.groups.map(function (group) {
+                                    return m(
+                                        'option',
+                                        { value: group.id() },
+                                        group.namePlural()
+                                    );
+                                })
+                            )
+                        )];
+                    }
+                }]);
+                return DefaultGroupSettingsModal;
+            })(SettingsModal);
 
-          babelHelpers.get(Object.getPrototypeOf(DefaultGroupSettingsModal.prototype), 'constructor', this).apply(this, args);
-
-          var user = app.session.user;
-
-          this.defaultGroup = this.setting('hyn.default_group.group') || Group.MEMBER_ID;
-          this.group = app.store.getById('groups', this.defaultGroup);
-
-          this.groups = app.store.all('groups').filter(function (group) {
-            return [Group.GUEST_ID].indexOf(group.id()) === -1;
-          });
+            _export('default', DefaultGroupSettingsModal);
         }
-
-        babelHelpers.createClass(DefaultGroupSettingsModal, [{
-          key: 'className',
-          value: function className() {
-            return 'DefaultGroupSettingsModal Modal--small';
-          }
-        }, {
-          key: 'title',
-          value: function title() {
-            return 'Default group Settings';
-          }
-        }, {
-          key: 'content',
-          value: function content() {
-            var _this = this;
-
-            return m(
-              'div',
-              { className: 'Modal-body', style: 'min-height: 400px' },
-              m(
-                'div',
-                { className: 'Form' },
-                m(
-                  'div',
-                  { className: 'Form-group' },
-                  m(
-                    'label',
-                    null,
-                    'Default group'
-                  ),
-                  m(
-                    'div',
-                    { className: 'Form-group' },
-                    this.groups.map(function (group) {
-                      return _this.radio(group);
-                    })
-                  )
-                )
-              )
-            );
-          }
-        }, {
-          key: 'radio',
-          value: function radio(option) {
-            var className = 'Checkbox ' + (option.id() == this.defaultGroup ? 'on' : 'off');
-            if (this.loading) className += ' loading';
-            return m(
-              'label',
-              { className: className },
-              m('input', { type: 'radio',
-                value: option.id(),
-                checked: option.id() == this.defaultGroup,
-                onchange: (m.withAttr('value'), this.save.bind(this))
-              }),
-              GroupBadge.component({ group: option }),
-              ' ',
-              option.namePlural()
-            );
-          }
-        }, {
-          key: 'save',
-          value: function save(e, groupId) {
-            var _this2 = this;
-
-            this.loading = true;
-            saveConfig({
-              'hyn.default_group.group': e.target.value
-            }).then(function () {
-              return _this2.hide();
-            }
-            //,
-            //() => m.redraw()
-            );
-          }
-        }]);
-        return DefaultGroupSettingsModal;
-      })(Modal);
-
-      _export('default', DefaultGroupSettingsModal);
-    }
-  };
+    };
 });;
 System.register('hyn/default-group/main', ['flarum/extend', 'flarum/app', 'hyn/default-group/components/DefaultGroupSettingsModal'], function (_export) {
   'use strict';
--- js/admin/src/components/DefaultGroupSettingsModal.js
+++ js/admin/src/components/DefaultGroupSettingsModal.js
@@ -1,77 +1,31 @@
-import Modal from 'flarum/components/Modal';
+import SettingsModal from 'flarum/components/SettingsModal';
 import Group from 'flarum/models/Group';
-import Button from 'flarum/components/Button';
-import saveConfig from 'flarum/utils/saveConfig';
 import SelectDropdown from 'flarum/components/SelectDropdown';
-import GroupBadge from 'flarum/components/GroupBadge';
 
-export default class DefaultGroupSettingsModal extends Modal {
-  constructor(...args) {
-    super(...args);
+export default class DefaultGroupSettingsModal extends SettingsModal {
+    className() {
+        return 'DefaultGroupSettingsModal Modal--small';
+    }
 
-    const user = app.session.user;
+    title() {
+        return 'Default group Settings';
+    }
 
-    this.defaultGroup = this.setting('hyn.default_group.group') || Group.MEMBER_ID;
-    this.group = app.store.getById('groups', this.defaultGroup);
-
-    this.groups = app.store.all('groups')
-        .filter(group => [Group.GUEST_ID].indexOf(group.id()) === -1);
-  }
-
-  className() {
-    return 'DefaultGroupSettingsModal Modal--small';
-  }
-
-  title() {
-    return 'Default group Settings';
-  }
-
-  content() {
-    return (
-      <div className="Modal-body" style="min-height: 400px">
-        <div className="Form">
-
-
-          <div className="Form-group">
-            <label>Default group</label>
+    form() {
+        this.defaultGroup = this.setting('hyn.default_group.group') || Group.MEMBER_ID;
+        this.groups = app.store.all('groups')
+            .filter(group => [Group.GUEST_ID].indexOf(group.id()) === -1);
+        return [
             <div className="Form-group">
-
-              {this.groups.map(group =>
-                this.radio(group)
-              )}
-
-
+                <label>Default group</label>
+                <select name="defaultGroup" bidi={this.defaultGroup}>
+                    {this.groups.map(group =>
+                    <option value={group.id()}>
+                        {group.namePlural()}
+                    </option>
+                    )}
+                </select>
             </div>
-          </div>
-
-        </div>
-      </div>
-    );
-  }
-
-  radio(option) {
-    let className = 'Checkbox ' + (option.id() == this.defaultGroup ? 'on' : 'off');
-    if (this.loading) className += ' loading';
-    return (
-      <label className={className}>
-        <input type="radio"
-          value={option.id()}
-          checked={option.id() == this.defaultGroup}
-          onchange={m.withAttr('value'), this.save.bind(this)}
-          />
-        {GroupBadge.component({group: option})} {option.namePlural()}
-      </label>
-    );
-  }
-
-  save(e, groupId) {
-    this.loading = true;
-    saveConfig({
-      'hyn.default_group.group': e.target.value
-    }).then(
-      () => this.hide()
-        //,
-      //() => m.redraw()
-    );
-  }
+        ];
+    }
 }
