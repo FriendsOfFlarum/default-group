22f193a5347797e9610ad5de22b4cbafdd1b5ee8
Ian Morland <ian@morland.me>
2021-11-26 02:12:18 +0000

    Housekeeping

--- js/src/admin/components/DefaultGroupSettingsPage.js
+++ js/src/admin/components/DefaultGroupSettingsPage.js
@@ -1,3 +1,4 @@
+import app from 'flarum/admin/app';
 import ExtensionPage from 'flarum/admin/components/ExtensionPage';
 import Button from 'flarum/common/components/Button';
 import Dropdown from 'flarum/common/components/Dropdown';
@@ -5,46 +6,46 @@ import icon from 'flarum/common/helpers/icon';
 import Group from 'flarum/common/models/Group';
 
 export default class DefaultGroupSettingsPage extends ExtensionPage {
-    oninit(vnode) {
-        super.oninit(vnode);
+  oninit(vnode) {
+    super.oninit(vnode);
 
-        this.selected = this.setting('fof-default-group.group');
-    }
+    this.selected = this.setting('fof-default-group.group');
+  }
 
-    content() {
-        const icons = {
-            3: 'fas fa-user',
-        };
-        const group = app.store.getById('groups', this.selected()) || app.store.getById('groups', Group.MEMBER_ID);
+  content() {
+    const icons = {
+      3: 'fas fa-user',
+    };
+    const group = app.store.getById('groups', this.selected()) || app.store.getById('groups', Group.MEMBER_ID);
 
-        return [
-            <div className="container">
-                <div className="FoFDefaultGroupSettingsPage">
-                    <div className="Form-group">
-                        <p>{app.translator.trans('fof-default-group.admin.settings.info')}</p>
+    return [
+      <div className="container">
+        <div className="FoFDefaultGroupSettingsPage">
+          <div className="Form-group">
+            <p>{app.translator.trans('fof-default-group.admin.settings.info')}</p>
 
-                        <Dropdown label={[icon(group.icon() || icons[group.id()]), '\t', group.namePlural()]} buttonClassName="Button Button--danger">
-                            {app.store
-                                .all('groups')
-                                .filter((g) => g.id() != 2)
-                                .map((g) =>
-                                    Button.component(
-                                        {
-                                            active: group.id() === g.id(),
-                                            disabled: group && group.id() === g.id(),
-                                            icon: g.icon() || icons[g.id()],
-                                            onclick: () => {
-                                                this.selected(g.id());
-                                            },
-                                        },
-                                        g.namePlural()
-                                    )
-                                )}
-                        </Dropdown>
-                    </div>
-                    {this.submitButton()}
-                </div>
-            </div>,
-        ];
-    }
+            <Dropdown label={[icon(group.icon() || icons[group.id()]), '\t', group.namePlural()]} buttonClassName="Button Button--danger">
+              {app.store
+                .all('groups')
+                .filter((g) => g.id() != 2)
+                .map((g) =>
+                  Button.component(
+                    {
+                      active: group.id() === g.id(),
+                      disabled: group && group.id() === g.id(),
+                      icon: g.icon() || icons[g.id()],
+                      onclick: () => {
+                        this.selected(g.id());
+                      },
+                    },
+                    g.namePlural()
+                  )
+                )}
+            </Dropdown>
+          </div>
+          {this.submitButton()}
+        </div>
+      </div>,
+    ];
+  }
 }
--- js/src/admin/index.ts
+++ js/src/admin/index.ts
@@ -1,5 +1,6 @@
+import app from 'flarum/admin/app';
 import DefaultGroupSettingsPage from './components/DefaultGroupSettingsPage';
 
 app.initializers.add('fof/default-group', () => {
-    app.extensionData.for('fof-default-group').registerPage(DefaultGroupSettingsPage);
+  app.extensionData.for('fof-default-group').registerPage(DefaultGroupSettingsPage);
 });
